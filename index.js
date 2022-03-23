741563448637efb1e1ca209ce86aa7aebed7b272
ZaharV <102241915+ZaharV@users.noreply.github.com>
2022-03-23 17:48:07 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,14 @@
+function Block(letter, rate, used, father, code) {
+  this.letter = letter;
+  this.rate = rate;
+  this.used = used;
+  this.father = father;
+  this.code = code;
+}
+
+function FindCode(block) {
+
+  if (tree[block.father].code
+   }
+  }
+ }
